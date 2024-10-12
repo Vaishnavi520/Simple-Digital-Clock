14c2f2b9abd17a1a824a3d35076abbe5c47cb498
Vaishnavi <125639025+Vaishnavi520@users.noreply.github.com>
2024-10-12 14:36:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
- var showtime=document.getElementsByTagName("p")[0].addEventListener("click",function(){
- showtime.classList.add("position");
+ document.getElementsByTagName("p")[0].addEventListener("click",function(){
+    var showtime=document.querySelector("p");
+    showtime.classList.add("position");
     setTimeout(function(){
   showtime.classList.remove("position");
- },100);
+ },150);
  
  var currentDate=new Date();
 
- document.querySelector("p").style.lineHeight="70px";this.style.fontSize="50px";this.style.marginTop="180px";this.style.color=" rgb(184, 114, 10)";this.style.fontFamily = " ' Roboto Mono', monospace";
+ document.querySelector("p").style.lineHeight="70px";this.style.fontSize="50px";this.style.marginTop="180px";this.style.color=" rgb(184, 114, 10)";this.style.fontFamily = " 'Roboto Mono', monospace";
  document.querySelector("p").innerHTML=currentDate.toLocaleString();
 
 setInterval(function(){
@@ -16,4 +17,4 @@ setInterval(function(){
 },1000);
 
 
- })
+ });
